4ab55a1e108d050a45473f9141fa4993f0c649bd
jingsong.zeng <jingsong.zeng@advance.ai>
2020-10-08 16:06:59 +0800

    split into Modules

--- index.js
+++ index.js
@@ -1,77 +1,29 @@
-const MyReact={createElement};
-const MyReactDom={render};
+import MyReact from './MyReact' 
+import MyReactDOM from './MyReactDom'
 
-function createElement(tag,attrs,...children){
-    return {
-        tag,
-        attrs,
-        children
-    }
+let num = 0
+let timer = null
+let styleObj = {
+  color: 'red',
+  fontSize: '20px'
 }
 
-let name = 'zhangsan'
-function clickBtn() {
+onStart() //一开始时执行
+
+function onStart() {
   console.log('click me')
+  timer = setInterval(() => { //启动时，每秒钟计时一次，做一次渲染
+    num++
+    MyReactDOM.render((
+      <div className="wrapper">
+        <h1 style = { styleObj }>Number: { num }</h1>
+        <button onClick = { onStart }>start</button>
+        <button onClick = { onPause }>pause</button>
+      </div>
+    ), document.querySelector('#app'))
+  }, 1000)
 }
 
-let element = (
-  <div className="wrapper">
-    <h1 style={{color:'#f00'}}>hello {name}</h1>
-    <button onClick = {clickBtn}>test</button>
-  </div>
-)
-
-function render(vnode, container) { //每次调用 render 时，先把之前的清空
-  container.innerHTML = ''
-  _render(vnode, container)
+function onPause() {
+  clearInterval(timer) //点击停止时，清除定时器
 }
-
-function _render(vnode, container) {
-  if (typeof vnode === 'string' || typeof vnode === 'number') { //如果是 string 或者 nubmer 都去创建文本节点
-    return container.appendChild(document.createTextNode(vnode))
-  }
-
-  if (typeof vnode === 'object') {
-    let dom = document.createElement(vnode.tag)
-    setAttribute(dom, vnode.attrs)
-    if (vnode.children && Array.isArray(vnode.children)) {
-      vnode.children.forEach(vnodeChild => {
-        _render(vnodeChild, dom) //记得这里是 _render , 这里的逻辑是不清空的
-      })
-    }
-
-    container.appendChild(dom)
-  }
-}
-
-
-function _render(vnode, container) {
-  if (typeof vnode === 'string' || typeof vnode === 'number') { //如果是 string 或者 nubmer 都去创建文本节点
-    return container.appendChild(document.createTextNode(vnode))
-  }
-
-  if (typeof vnode === 'object') {
-    let dom = document.createElement(vnode.tag)
-    setAttribute(dom, vnode.attrs)
-    if (vnode.children && Array.isArray(vnode.children)) {
-      vnode.children.forEach(vnodeChild => {
-        _render(vnodeChild, dom) //记得这里是 _render , 这里的逻辑是不清空的
-      })
-    }
-
-    container.appendChild(dom)
-  }
-}
-
-function setAttribute(dom, attrs) {
-  for(let key in attrs) {
-    if(/^on/.test(key)) { //对事件绑定的处理，以 on 开头的，dom[onclick] = attrs[onClick]
-      dom[key.toLocaleLowerCase()] = attrs[key]
-    }
-    if(key === 'style') { //对 style 的处理
-      Object.assign(dom.style, attrs[key])
-    }
-  }
-}
-
-MyReactDom.render(element,document.getElementById('root'))
